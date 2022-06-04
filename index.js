f7efc635a5f2bc091b42abada77034e71ed92ca1
wanda <wandaul16@gmail.com>
2022-06-04 16:37:29 +0700

    challenge 05

--- index.js
+++ index.js
@@ -6,16 +6,29 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+const sorter = (names, operation) => {
+
+      const newArr = [];
+      const result = operation(names);
+      
+      for(let i=1; i <= result.length; i++) {
+         newArr.push(`${i}. ${result[i-1]}`);
+      }
+
+     return newArr;
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (names) => {
+   return names.sort();
+}
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
-
+const sortDescending = (names) => {
+  return names.sort().reverse();
+}
 // ! JANGAN DIMODIFIKASI
 (function main() {
   console.log(sorter?.(names, sortAscending)?.join("\n"));
